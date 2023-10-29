9af2c2f0022428173c6aeab82a2b51e11fdbfafc
Pedro Guemes <saldunajose@gmail.com>
2023-10-29 13:10:12 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: 'Which is the largest animal in the world?',
+    answers: [
+      { text: 'Shark', correct: false },
+      { text: 'Blue whale', correct: true },
+      { text: 'Giant turtle', correct: false },
+      { text: 'Elephant', correct: false },
+    ],
+  },
+  {
+    question: 'Which is the largest desert in the world?',
+    answers: [
+      { text: 'Kalahari', correct: false },
+      { text: 'Gobi', correct: false },
+      { text: 'Antartica', correct: true },
+      { text: 'Sahara', correct: false },
+    ],
+  },
+  {
+    question: 'Wich is the smallest continet in the world?',
+    answers: [
+      { text: 'Australia', correct: true },
+      { text: 'Asia', correct: false },
+      { text: 'Africa', correct: false },
+      { text: 'America', correct: false },
+    ],
+  },
+  {
+    question: 'Wich is the smallest country in the world?',
+    answers: [
+      { text: 'Sri Lanka', correct: false },
+      { text: 'Nepal', correct: false },
+      { text: 'Bhutan', correct: false },
+      { text: 'Vatican city', correct: true },
+    ],
+  },
+];
